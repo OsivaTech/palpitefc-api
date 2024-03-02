13d9c746763ec329705b2f83ec6f1a3f1739cab6
Francisco Augusto dos Santos <santos.frau@gmail.com>
2024-03-02 16:04:37 -0300

    first commit

---
+++ src/Api/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("cors", policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseCors("cors");
+
+app.MapGet("/echo", () =>
+{
+    return "hello world";
+});
+
+app.Run();
+
