7858272a439506429eac9a7d45e4c8b444555bd6
Francisco Augusto dos Santos <santos.frau@gmail.com>
2024-03-02 16:27:09 -0300

    feat(ExceptionHander) - Adds GlobalExceptionHandler

---
+++ src/Api/ExceptionHandlers/GlobalExceptionHandler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Api.ExceptionHandlers;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    #region Fields
+
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+
+    #endregion
+
+    #region Constructors
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var problemDetails = new ProblemDetails
+        {
+            Status = (int)HttpStatusCode.InternalServerError,
+            Type = exception.GetType().Name,
+            Title = "An unexpected error occurred",
+            Detail = exception.Message,
+            Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}",
+            Extensions = new Dictionary<string, object?>
+            {
+                { "TraceKey", httpContext.TraceIdentifier }
+            }
+        };
+
+        _logger.LogError(exception, "Exception occurred: {Message}", exception.Message);
+
+        await Results.Problem(problemDetails).ExecuteAsync(httpContext);
+
+        return true;
+    }
+
+    #endregion
+}
