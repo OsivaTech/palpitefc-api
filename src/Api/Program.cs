639da9cf97b8a6e8e23047d45591acc721620c00
Francisco Augusto dos Santos <santos.frau@gmail.com>
2024-03-23 20:51:43 -0300

    feat(Log) - Adds SerialLog lib to send logs to Grafana Loki

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -3,53 +3,81 @@ using PalpiteFC.Api.ExceptionHandlers;
 using PalpiteFC.Api.Extensions;
 using PalpiteFC.Api.Integrations.Extensions;
 using PalpiteFC.Api.Middlewares;
+using Serilog;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddCors(options =>
+try
 {
-    options.AddPolicy("cors", policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
-});
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console(outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss} [{Level:u3}] [{ThreadId}] {Message}{NewLine}{Exception}")
+        .CreateLogger();
 
-builder.Services.ConfigureHttpJsonOptions(options =>
+    var builder = WebApplication.CreateBuilder(args);
+
+    Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
+
+    Log.Information("Configuring app.");
+
+    builder.Host.UseSerilog(Log.Logger);
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("cors", policy =>
+            {
+                policy.AllowAnyOrigin()
+                      .AllowAnyHeader()
+                      .AllowAnyMethod();
+            });
+    });
+
+    builder.Services.ConfigureHttpJsonOptions(options =>
+    {
+        options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.SerializerOptions.PropertyNameCaseInsensitive = true;
+        options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.SerializerOptions.Converters.Add(new DateTimeConverter());
+    });
+
+    builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+    builder.Services.AddProblemDetails();
+
+    builder.Services.ConfigureValidators();
+    builder.Services.ConfigureDependencyInjection();
+    builder.Services.AddIntegrationServices(builder.Configuration);
+    builder.Services.ConfigureIOptions(builder.Configuration);
+    builder.Services.ConfigureApiSecurity(builder.Configuration);
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    app.UsePathBase(new PathString("/api"));
+    app.UseRouting();
+
+    app.UseCors("cors");
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.InitiaizeDatabase();
+    app.UseMiddleware<UserContextMiddleware>();
+    app.MapEndpoints();
+    app.UseExceptionHandler();
+
+    Log.Information("App configured. Starting...");
+
+    app.Run();
+}
+catch (Exception ex)
 {
-    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-    options.SerializerOptions.PropertyNameCaseInsensitive = true;
-    options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    options.SerializerOptions.Converters.Add(new DateTimeConverter());
-});
-
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
-
-builder.Services.ConfigureValidators();
-builder.Services.ConfigureDependencyInjection();
-builder.Services.AddIntegrationServices(builder.Configuration);
-builder.Services.ConfigureIOptions(builder.Configuration);
-builder.Services.ConfigureApiSecurity(builder.Configuration);
-
-var app = builder.Build();
-
-app.UsePathBase(new PathString("/api"));
-app.UseRouting();
-
-app.UseCors("cors");
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.InitiaizeDatabase();
-app.UseMiddleware<UserContextMiddleware>();
-app.MapEndpoints();
-app.UseExceptionHandler();
-
-app.Run();
+    Log.Fatal(ex, "Host terminated unexpectedly: {Message}", ex.Message);
+}
+finally
+{
+    Log.Information("Server Shutting down...");
+    Log.CloseAndFlush();
+}
