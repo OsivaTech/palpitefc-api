60a0120b80948ebf4bb768b08ce920d4f31f198c
Francisco Augusto dos Santos <santos.frau@gmail.com>
2024-06-27 21:22:17 -0300

    feat(waitingList): adds phoneNumber field

--- src/Application/Requests/WaitingListRequest.cs
+++ src/Application/Requests/WaitingListRequest.cs
@@ -7,6 +7,7 @@ public class WaitingListRequest
     public string? Name { get; set; }
     public string? Email { get; set; }
     public string? Team { get; set; }
+    public string? PhoneNumber { get; set; }
     public Gender? Gender { get; set; }
     public DateOnly? Birthday { get; set; }
     public int Age { get; set; }
